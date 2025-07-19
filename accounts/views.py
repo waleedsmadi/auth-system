6133ebccc4a3bbee3afdaa1ea298396daf5e3715
Waleed Smadi <waleed0smadi@outlook.com>
2025-07-20 01:09:09 +0300

    fix(SignUp) Fix a typo in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,7 @@ def send_token_email(user):
 def resend_token_email(request, token):
     user = MyUser.objects.get(email_token=token)
     if user.is_verified:
-        return render(request, 'accounts/activation_status.html', {'verified': 'The  accountis activated'})
+        return render(request, 'accounts/activation_status.html', {'verified': 'The Account Is Activated'})
     
     user.email_token = uuid4()
     user.creation_date = timezone.now()
