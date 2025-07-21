f36bac381056ef27f4122cdccabd7590b5e0c54e
Waleed Smadi <waleed0smadi@outlook.com>
2025-07-21 21:20:33 +0300

    view all posts in pages/templates/pages/index.html

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from posts.models import Post
 
 def index(request):
-    return render(request, 'pages/index.html')
+    posts = Post.objects.all()
+    return render(request, 'pages/index.html', {'posts': posts})
