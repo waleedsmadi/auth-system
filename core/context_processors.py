5836a07c08ef8a5296c66b4155b638d78fd3afed
Waleed Smadi <waleed0smadi@outlook.com>
2025-07-22 16:52:46 +0300

    Add 'My Progile' choise in the navbar
     1 - change context_processors.py to return the user rather than status
    
     2 - make url for 'Home' and 'navbar-brand' links
    
     3 - view user's name on the navbar

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,3 +1,11 @@
-def logged_in_status(request):
-    status = request.session.get('user_id')
-    return {'is_logged_in': status}
+from accounts.models import MyUser
+
+def logged_in_user(request):
+    user_id = request.session.get('user_id')
+    if user_id:
+        try:
+            user = MyUser.objects.get(id=user_id)
+            return {'logged_user': user}
+        except MyUser.DoesNotExist:
+            pass
+    return {'logged_user': None}
